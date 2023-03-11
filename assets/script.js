cf10b3e9412c0936f72ad241fafeb5c79d005410
Eduardo Mandujano <mandujanolalo@gmail.com>
2023-03-10 21:42:28 -0800

    get api, local storage, search button event listener

--- assets/script.js
+++ assets/script.js
@@ -23,4 +23,29 @@ var searchSubmitHandler = function(event){
     } else if (userInput == ""){
         alert("Please enter a city");
     }
+
 };
+
+async function getCityWeather(){
+    var apiURL = api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid={e5235fdc998e76e5565b34ce5b174f5f
+    }
+}
+
+
+function setLocalStorage(city){
+    if (recentSearches.indexOf(city) === 1 ){
+        recentSearches.push(city);
+        localStorage.setItem("recent", JSON.stringify(recentSearches));
+    }
+}
+
+searchBtn.addEventListener("click",()=>{
+    var userInput= inputValue.val().trim();
+    if (userInput !== ""){
+        getCityWeather(searchInput.value);
+        renderRecents();
+        inputValue.val("");
+    } else if (userInput == ""){
+        alert('Please enter a city please')
+    }
+})

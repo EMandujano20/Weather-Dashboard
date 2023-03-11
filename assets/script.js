eab1ab9faf32d1683a3ddc0276aec510df5e1a7f
Eduardo Mandujano <mandujanolalo@gmail.com>
2023-03-10 22:29:10 -0800

    api key

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,5 @@
-//var weatherApiRootUrl = 
+var weatherApiRootUrl = 'https://api.openweathermap.org';
+var weatherApiKey = 'e5235fdc998e76e5565b34ce5b174f5f';
 
 var searchInput = document.querySelector(".inputValue");
 var searchBtn = document.querySelector(".searchBtn");
@@ -13,6 +14,7 @@ var recentContainer = $("#recent");
 var inputValue = $("#inputValue");
 
 
+
 var searchSubmitHandler = function(event){
     event.preventDefault;
 
@@ -25,12 +27,43 @@ var searchSubmitHandler = function(event){
     }
 
 };
+async function getWeather(city) {
+    var apiUrl =
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+      city +
+      "&units=imperial&appid=e5235fdc998e76e5565b34ce5b174f5f";
+                             
+    var response = await fetch(apiUrl);
+    if (response.ok) {
+      var data = await response.json();
+      var nameValue = data.name;
+      var tempValue = data.main.temp;
+      var humidityValue = data.main.humidity;
+      var windValue = data.wind.speed;
+      console.log(data);
+      var lat = data.coord.lon;
+      var lon = data.coord.lat;
+      await uvIndex(data.coord.lat, data.coord.lon);
+      var icon = data.weather[0].icon;
 
-async function getCityWeather(){
-    var apiURL = api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid={e5235fdc998e76e5565b34ce5b174f5f
-    }
-}
+      cityDateIcon.innerHTML =
+      nameValue + currentDate.format(" (M/DD/YYYY) ") + icon;
+      temp.innerHTML = "Temperature: " + tempValue + " °F";
+      humidity.innerHTML = "Humidity: " + humidityValue + "%";
+      wind.innerHTML = "Wind Speed: " + windValue + " MPH";
+    }};
 
+
+  
+    var response = await fetch(apiUrl);
+    if (response.ok) {
+      var data = await response.json();
+      var nameValue = data.name;
+      var tempValue = data.main.temp;
+      var humidityValue = data.main.humidity;
+      var windValue = data.wind.speed;
+      console.log(data);
+    };
 
 function setLocalStorage(city){
     if (recentSearches.indexOf(city) === 1 ){
@@ -48,4 +81,6 @@ searchBtn.addEventListener("click",()=>{
     } else if (userInput == ""){
         alert('Please enter a city please')
     }
-})
+});
+
+
